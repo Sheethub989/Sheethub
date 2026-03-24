41e519e859973aab8a38669f508b86c11424532d
Sheet Hub <sheethub989@gmail.com>
2026-03-25 01:34:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,8 +191,6 @@ if st.session_state.user_id is None:
                 st.session_state.email = email
                 st.rerun()
 
-        st.markdown('</div>', unsafe_allow_html=True)
-
     st.stop()
 
 # ---------------- USER ----------------
